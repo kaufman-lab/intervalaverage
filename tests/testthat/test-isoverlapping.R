f1d01b6cc121cdf151718cf6bf84b0bb8e1a4d0b
Michael Young <myoung3@deohs-mesa03.deohs.washington.edu>
2020-09-12 17:34:26 -0700

    remove intervalaveragev function since it was too slow for large number of groups

--- tests/testthat/test-isoverlapping.R
+++ tests/testthat/test-isoverlapping.R
@@ -5,38 +5,32 @@ test_that("is.overlapping", {
                "values in columns specified by interval_vars must be non-missing")
 
 
-  expect_error(is.overlappingv(x$a,x$b),
-               "missing")
-
 
   x <- data.table(a=c(NA,5L),b=c(2L,5L))
   expect_error(is.overlapping(x, c("a","b")),
                "values in columns specified by interval_vars must be non-missing")
 
-  expect_error(is.overlappingv(x$a,x$b),
-               "missing")
 
   x <- data.table(a=c(5L,1L),b=c(2L,1L))
   expect_error(is.overlapping(x, c("a","b")),
                "interval_vars must specify columns corresponding to increasing intervals")
 
-  expect_error(is.overlappingv(x$a,x$b))
 
   x <- data.table(a=c(1L,5L),b=c(6L,10L))
   expect_true(is.overlapping(x, c("a","b")))
-  expect_true(is.overlappingv(x$a,x$b))
+
 
   x <- data.table(a=c(5L,1L),b=c(10L,6L))
   expect_true(is.overlapping(x, c("a","b")))
-  expect_true(is.overlappingv(x$a,x$b))
+
 
   x <- data.table(a=c(1L,3L,5L,-3L),b=c(2L,4L,6L,2L))
   expect_true(is.overlapping(x, c("a","b")))
-  expect_true(is.overlappingv(x$a,x$b))
+
 
   x <- data.table(a=c(1L,3L,5L,20L,7L,-1e9L),b=c(2L,4L,6L,300000L,10L,0L))
   expect_false(is.overlapping(x, c("a","b")))
-  expect_false(is.overlappingv(x$a,x$b))
+
 
 
   x <- data.table(a=c(1L,3L,5L,20L,7L,-1e9L),b=c(2L,4L,6L,300000L,10L,0L),id=c(1,1,1,2,2,2))
